6b90ed336eb5ed1395a554c8b3c828dc1c8fe7c7
Nariod <dorian.p@protonmail.ch>
2023-02-12 18:16:00 +0100

    testing clap v4

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@ mod error;
 pub use error::Error;
 use std::{env, fs::File, io::{BufReader, BufRead}, time::Duration};
 use reqwest::Client;
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    target_url: String,
+    #[arg(short, long)]
+    path_to_wordlist: String,
+ }
 
 async fn forcer(http_client: &Client, target: &str) -> Result<bool, reqwest::Error> {
     let response = http_client
@@ -18,8 +29,16 @@ async fn forcer(http_client: &Client, target: &str) -> Result<bool, reqwest::Err
     }
 }
 
+
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    let args = Args::parse();
+
+    let target = args.target_url;
+    let wordlist_file = File::open(args.path_to_wordlist)?;
+
+    /* 
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
@@ -27,6 +46,7 @@ async fn main() -> Result<(), anyhow::Error> {
     }
     let target = args[1].as_str();
     let wordlist_file = File::open(&args[2])?;
+    */
     let reader = BufReader::new(&wordlist_file);
     let http_timeout = Duration::from_secs(10);
     let http_client = Client::builder().timeout(http_timeout).build()?;
