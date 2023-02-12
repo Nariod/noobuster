4d23f460ce29b05064676f05fc4d3045c381de70
Nariod <dorian.p@protonmail.ch>
2023-02-12 21:45:06 +0100

    tokio to debug

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{
     io::{BufRead, BufReader},
     time::Duration,
 };
+use tokio::task;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -17,8 +18,8 @@ struct Args {
     path_to_wordlist: String,
 }
 
-async fn forcer(http_client: &Client, target: &str) -> Result<bool, reqwest::Error> {
-    let response = http_client.get(target).send().await?.status();
+async fn forcer(http_client: Client, target: String) -> Result<bool, reqwest::Error> {
+    let response = http_client.get(&target).send().await?.status();
 
     if response == 200 {
         Ok(true)
@@ -30,33 +31,26 @@ async fn forcer(http_client: &Client, target: &str) -> Result<bool, reqwest::Err
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
-
     let target = args.target_url;
     let wordlist_file = File::open(args.path_to_wordlist)?;
 
-    /*
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 3 {
-        return Err(Error::CliUsage.into());
-    }
-    let target = args[1].as_str();
-    let wordlist_file = File::open(&args[2])?;
-    */
     let reader = BufReader::new(&wordlist_file);
     let http_timeout = Duration::from_secs(10);
     let http_client = Client::builder().timeout(http_timeout).build()?;
 
+
+
     for line in reader.lines() {
         let line = line?;
         let common_filename = line.trim();
-        let build = format!("{target}/{common_filename}");
+        let build = format!("{target}{common_filename}");
 
-        let lol = forcer(&http_client, &build).await?;
-
-        if lol == true {
-            println!("[200] {}", format!("{target}/{common_filename}"));
-        }
+        tokio::spawn(async move {
+            let lol = forcer(http_client, build).await?;
+            if lol {
+                println!("[200] {build}");
+            }
+        });
     }
 
     Ok(())
