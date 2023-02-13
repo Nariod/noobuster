77e856958641df6c999486c4d845fe00bce73c40
Nariod <dorian.p@protonmail.ch>
2023-02-13 08:38:28 +0100

    tokio working

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use std::{
     io::{BufRead, BufReader},
     time::Duration,
 };
-use tokio::task;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -18,9 +17,8 @@ struct Args {
     path_to_wordlist: String,
 }
 
-async fn forcer(http_client: Client, target: String) -> Result<bool, reqwest::Error> {
-    let response = http_client.get(&target).send().await?.status();
-
+async fn forcer(http_client: Client, target: &str) -> Result<bool, reqwest::Error> {
+    let response = http_client.get(target).send().await?.status();
     if response == 200 {
         Ok(true)
     } else {
@@ -35,22 +33,31 @@ async fn main() -> Result<(), anyhow::Error> {
     let wordlist_file = File::open(args.path_to_wordlist)?;
 
     let reader = BufReader::new(&wordlist_file);
-    let http_timeout = Duration::from_secs(10);
+    let http_timeout = Duration::from_secs(5);
     let http_client = Client::builder().timeout(http_timeout).build()?;
 
-
+    let mut handles = vec![];
 
     for line in reader.lines() {
         let line = line?;
         let common_filename = line.trim();
         let build = format!("{target}{common_filename}");
+        let http_client = http_client.clone();
 
-        tokio::spawn(async move {
-            let lol = forcer(http_client, build).await?;
-            if lol {
-                println!("[200] {build}");
+        let handle = tokio::spawn(async move {
+            let result = forcer(http_client, &build).await;
+
+            if let Ok(content) = result {
+                if content {
+                    println!("[200] {build}");
+                }
             }
         });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await?;
     }
 
     Ok(())
